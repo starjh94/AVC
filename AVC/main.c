01cec09fcf25677b6e78e7477d89bb5daa641bc1
starjh94 <bluesongy@gmail.com>
2016-05-25 12:54:12 +0900

    Initial Commit

---
+++ AVC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AVC
+//
+//  Created by 유진후 on 2016. 5. 25..
+//  Copyright © 2016년 유진후. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
